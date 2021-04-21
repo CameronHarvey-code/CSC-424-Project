18e032d9e82bb347f14d8ace3398524697db51da
Dino_Henriquez <dinohenriquez@gmail.com>
2021-04-20 21:30:32 -0500

    Add files via upload

--- Server/Database/Queries.js
+++ Server/Database/Queries.js
@@ -1,4 +1,4 @@
-const UpdAtt = "UPDATE sunday_service SET nursery = ?, pre_school = ?, kinder = ?, early_elementary = ?, late_elementary = ? WHERE event_id = ?"
+const UpdAtt = "UPDATE sunday_service SET nursery = ?, pre_school = ?, kinder = ?, early_elementary = ?, late_elementary = ?, teachers_assistants = ?, sanctuary = ?, media_room = ?, ushers = ?, visitors = ?, w_baptisms = ?, h_g_baptisms = ? WHERE event_id = ?"
 const Register = "INSERT INTO user (user_email, user_password, user_firstname, user_lastname, user_status, user_id) VALUES (?,?,?,?,?, uuid())"
 const Login = "SELECT * FROM user WHERE user_email = ? "
 const AddEvent = "INSERT INTO sunday_service (event_id, event_name, date) VALUES (uuid(),?,?)";
